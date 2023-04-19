bb00223d4253d795e0ebbc10091cdf2ac85d769b
merry <k.maria.m.24.05@gmail.com>
2023-04-19 15:31:25 +0300

    Update 6 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,9 @@ const inputElDataLength = parseInt(inputEl.getAttribute('data-length'));
 
 const inputElValidation = event => {
     const inputElValue = event.currentTarget.value;
-    inputElValue.length === inputElDataLength ? inputEl.classList.add('valid') : inputEl.classList.add('invalid');
-    return;
+    const inputElDataLengthIsOk = inputElValue.length === inputElDataLength;
+    inputElDataLengthIsOk ? inputEl.classList.add('valid') & inputEl.classList.remove('invalid'): inputEl.classList.add('invalid') & inputEl.classList.remove('valid');
 };
 
 inputEl.addEventListener('blur', inputElValidation);
-
 
